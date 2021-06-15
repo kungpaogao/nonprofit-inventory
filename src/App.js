fa2740a5fb20c0f2392d0fbc963a0be40a6cfd37
Andrew <kungpaogao@gmail.com>
2021-06-15 02:13:43 -0400

    Initial app concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [fieldName, setFieldName] = useState(null);
+  const [fieldType, setFieldType] = useState(null);
+
+  const [formObject, setFormObject] = useState({});
+
+  function addField() {
+    if (fieldName && fieldType) {
+      const newFormObject = { ...formObject };
+      newFormObject[fieldName] = fieldType;
+      setFormObject(newFormObject);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.addField}>
+        <h2>Add element</h2>
+        <div className={styles.formField}>
+          <label>
+            Field name
+            <br />
+            <input onChange={(e) => setFieldName(e.target.value)} />
+          </label>
+        </div>
+        <div className={styles.formField}>
+          <label>
+            Field type
+            <br />
+            <select onChange={(e) => setFieldType(e.target.value)}>
+              <option value="text">text</option>
+              <option value="number">number</option>
+              <option value="date">date</option>
+              <option value="select">select</option>
+            </select>
+          </label>
+        </div>
+        <div className={styles.formField}>
+          <label></label>
+        </div>
+        <div>
+          {fieldName}: {fieldType}
+        </div>
+        <button onClick={addField}>Add field</button>
+      </div>
+      <div className={styles.results}>
+        <h2>Results</h2>
+        <pre>{JSON.stringify(formObject, null, 2)}</pre>
+      </div>
     </div>
   );
 }
