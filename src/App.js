ffb5596eb60da4d637d9ae7a04f49dae177e0cc3
Andrew <kungpaogao@gmail.com>
2021-06-16 01:49:20 -0400

    Add field options; simplify styling

--- src/App.js
+++ src/App.js
@@ -2,48 +2,75 @@ import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  const [fieldName, setFieldName] = useState(null);
-  const [fieldType, setFieldType] = useState(null);
+  const [fieldName, setFieldName] = useState("");
+  const [fieldType, setFieldType] = useState("");
+  const [fieldOptions, setFieldOptions] = useState("");
 
   const [formObject, setFormObject] = useState({});
 
   function addField() {
     if (fieldName && fieldType) {
       const newFormObject = { ...formObject };
-      newFormObject[fieldName] = fieldType;
+      if (requireOptions()) {
+        if (!fieldOptions) return;
+        newFormObject[fieldName] = `${fieldType}:${fieldOptions}`;
+      } else {
+        newFormObject[fieldName] = fieldType;
+      }
       setFormObject(newFormObject);
+      setFieldName("");
+      setFieldOptions("");
     }
   }
+
+  const requireOptions = () => fieldType === "radio" || fieldType === "select";
 
   return (
     <div className={styles.app}>
       <div className={styles.addField}>
-        <h2>Add element</h2>
-        <div className={styles.formField}>
+        <h2>Add field</h2>
+        <label>
+          Field name
+          <br />
+          <input
+            onChange={(e) => setFieldName(e.target.value)}
+            value={fieldName}
+          />
+        </label>
+        <label>
+          Field type
+          <br />
+          <select
+            onChange={(e) => setFieldType(e.target.value)}
+            value={fieldType}
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            <option value="text">Text</option>
+            <option value="select">Select</option>
+            <option value="checkbox">Checkbox</option>
+            <option value="radio">Radio</option>
+            <option value="number">Number</option>
+            <option value="textarea">Textarea</option>
+            <option value="email">Email</option>
+            <option value="tel">Tel</option>
+            <option value="url">URL</option>
+            <option value="time">Time</option>
+            <option value="datetime">Datetime</option>
+          </select>
+        </label>
+        {requireOptions() && (
           <label>
-            Field name
+            Options
             <br />
-            <input onChange={(e) => setFieldName(e.target.value)} />
+            <input
+              onChange={(e) => setFieldOptions(e.target.value)}
+              value={fieldOptions}
+              placeholder="Enter options separate by ;"
+            />
           </label>
-        </div>
-        <div className={styles.formField}>
-          <label>
-            Field type
-            <br />
-            <select onChange={(e) => setFieldType(e.target.value)}>
-              <option value="text">text</option>
-              <option value="number">number</option>
-              <option value="date">date</option>
-              <option value="select">select</option>
-            </select>
-          </label>
-        </div>
-        <div className={styles.formField}>
-          <label></label>
-        </div>
-        <div>
-          {fieldName}: {fieldType}
-        </div>
+        )}
         <button onClick={addField}>Add field</button>
       </div>
       <div className={styles.results}>
