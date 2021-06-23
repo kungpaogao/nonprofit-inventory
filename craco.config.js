7eb8453014363a8313607e6c96f434e7dbf27049
Andrew <kungpaogao@gmail.com>
2021-06-23 01:39:02 -0400

    Add TODO for minimizing bundle size

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,6 @@
+// TODO: https://material-ui.com/guides/minimizing-bundle-size/#development-environment
+// - https://github.com/gsoft-inc/craco
+// - https://github.com/jackwilsdon/craco-use-babelrc
 module.exports = {
   style: {
     postcss: {
