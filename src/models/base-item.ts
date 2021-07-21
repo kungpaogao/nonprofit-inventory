e6a958a3e2558a0a306b3ceac9222fe1632f0f63
Andrew <kungpaogao@gmail.com>
2021-07-20 21:50:58 -0400

    Get inventory data from Firestore

---
+++ src/models/base-item.ts
@@ -0,0 +1,5 @@
+export type BaseItem = {
+  id: string;
+  schema: string;
+  version: string;
+};
--- src/ui/Inventory/index.tsx
+++ src/ui/Inventory/index.tsx
@@ -1,51 +1,42 @@
 import { DataGrid } from "@material-ui/data-grid";
+import {
+  useCollection,
+  useDocumentDataOnce,
+} from "react-firebase-hooks/firestore";
+import { Schema } from "../../models/schema";
+import { db } from "../../network/firebase";
+
+const MODEL = "furniture";
 
 export default function Inventory() {
-  const columns = [
-    { field: "class", headerName: "Class", flex: 1 },
-    { field: "status", headerName: "Status", flex: 1 },
-    { field: "zone", headerName: "Zone", flex: 1 },
-    { field: "address", headerName: "Address", flex: 1 },
-    { field: "donor", headerName: "Donor", flex: 1 },
-    { field: "dateAdded", headerName: "Date Added", flex: 1 },
-  ];
+  const [schema, isSchemaLoading, isSchemaError] = useDocumentDataOnce<Schema>(
+    db.doc(`schemas/${MODEL}/schemas/latest`)
+  );
 
-  const rows = [
-    {
-      id: "r1",
-      class: "Chair",
-      status: "OnTruck",
-      zone: "Ithaca",
-      address: "123 Cornell St Ithaca, NY",
-      donor: "Andrew",
-      dateAdded: "2021-06-28",
-    },
-    {
-      id: "r2",
-      class: "Chair",
-      status: "OnTruck",
-      zone: "Ithaca",
-      address: "123 Cornell St Ithaca, NY",
-      donor: "Andrew",
-      dateAdded: "2021-06-28",
-    },
-    {
-      id: "r3",
-      class: "Chair",
-      status: "OnTruck",
-      zone: "Ithaca",
-      address: "123 Cornell St Ithaca, NY",
-      donor: "Andrew",
-      dateAdded: "2021-06-28",
-    },
-  ];
+  const [data, isDataLoading, isDataError] = useCollection(
+    db.collection("data").where("schema", "==", MODEL)
+  );
+
+  const columns = schema?.schema.map(({ id, name }) => ({
+    field: id,
+    headerName: name,
+    flex: 1,
+  }));
+
+  if (isSchemaError || isDataError) {
+    return <div>Error.</div>;
+  }
+
+  if (isSchemaLoading || isDataLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="flex h-full w-full">
       <div className="flex-grow">
         <DataGrid
-          columns={columns}
-          rows={rows}
+          columns={columns!}
+          rows={data!.docs.map((doc) => doc.data())}
           pageSize={20}
           checkboxSelection
         />
